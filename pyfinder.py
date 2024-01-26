82ce4639900087af57347b4cce5c0c3dcb565571
abdallahEI12 <ei12abdallah@gmail.com>
2024-01-26 14:24:32 +0200

    hehe

--- pyfinder.py
+++ pyfinder.py
@@ -1,27 +1,27 @@
 import pygame
 import cv2
 from hs import *
-WINDOW_WIDTH = int(1.5 * 812)
-WINDOW_HEIGHT = int(1.5* 531)
-TOTAL_ROWS = 265
-TOTAL_COLS = 406
+WW = int(1.5 * 812)
+WH = int(1.5* 531)
+TTL_R = 265
+TTL_C = 406
 def main():
-    window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+    window = pygame.display.set_mode((WW,WH))
     pygame.display.set_caption("A* Path Finding Algorithm")
     #replace with you file path the image
     image = cv2.imread(r'D:\educational\projects\Graduation Project\pyfinder\edited.png')
-    grid = make_grid(TOTAL_COLS,TOTAL_ROWS,WINDOW_WIDTH,WINDOW_HEIGHT,image)
+    grid = make_grid(TTL_C,TTL_R,WW,WH,image)
     py_game_running = True
     start = None
     destination = None
     while py_game_running:
-        draw(window,grid,TOTAL_ROWS,TOTAL_COLS,WINDOW_WIDTH,WINDOW_HEIGHT)
+        draw(window,grid,TTL_R,TTL_C,WW,WH)
         for event in pygame.event.get():
             if event.type== pygame.QUIT:
                 py_game_running = False
             if pygame.mouse.get_pressed()[0]:
                 position = pygame.mouse.get_pos()
-                row, col = get_clicked_pos(position,TOTAL_ROWS,TOTAL_COLS,WINDOW_WIDTH,WINDOW_HEIGHT)
+                row, col = get_clicked_pos(position,TTL_R,TTL_C,WW,WH)
                 pressed_node = grid[row][col]
                 if not start and pressed_node != destination:
                     pressed_node.start = True
@@ -33,7 +33,7 @@ def main():
                     pressed_node.clr = BLUE
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and start and destination:
-                    path_list = astar(start,destination,grid,TOTAL_ROWS,TOTAL_COLS)
+                    path_list = astar(start,destination,grid,TTL_R,TTL_C)
 
 
                     print(path_list)
